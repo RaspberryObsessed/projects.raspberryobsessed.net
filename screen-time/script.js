596e285086f8e5810a258bcaffdfbae49a81a115
RaspberryObsessed <110307769+RaspberryObsessed@users.noreply.github.com>
2023-09-29 20:16:57 -0700

    Delete screen-time directory

--- screen-time/script.js
+++
@@ -1,8 +0,0 @@
-// Get references to the "Ask for more time" element and the popup menu
-const askForMoreTime = document.querySelector('.ask-for');
-const popupMenu = document.getElementById('popupMenu');
-
-// Toggle the popup menu when "Ask for more time" is clicked
-askForMoreTime.addEventListener('click', () => {
-    popupMenu.style.display = 'block';
-});
